6a9a27c68c75ed6474cc09e836ffc1ed8cd534d4
TG-NAV <laibaidu@outlook.com>
2025-01-08 08:45:26 +0800

    Update G.js

--- assets/website/js/frontend/G.js
+++ assets/website/js/frontend/G.js
@@ -72,11 +72,11 @@
     		return url;
     	},
     	CreateItem: function(time){
-    		var tempDate = G.Fun.formatter_date(time)
-    		tempDateSplit = tempDate.split('/clashnode/');
-    		var title = tempDateSplit[1] + '月'+ tempDateSplit[2] +'日更新，'+tempDateSplit[0]+'年最新高速Shadowrocket/SSR/V2ray/Clash订阅链接免费节点订阅';
+    		var tempDate = /clashnode/G.Fun.formatter_date(time)
+    		tempDateSplit = /clashnode/tempDate.split('/');
+    		var title = /clashnode/tempDateSplit[1] + '月'+ tempDateSplit[2] +'日更新，'+tempDateSplit[0]+'年最新高速Shadowrocket/SSR/V2ray/Clash订阅链接免费节点订阅';
     		var url = '/clashnode/newly-discovered-nodes/index.html?date=' + tempDate.replace(/\//g, '-');
-    		var time = tempDateSplit[1] + '月'+ tempDateSplit[2] +'日';
+    		var time = /clashnode/tempDateSplit[1] + '月'+ tempDateSplit[2] +'日';
 
     		return {title: title, url: url, time: time};
     	},
