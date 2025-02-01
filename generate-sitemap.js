d522a86d8ff363fe0e6e46aa28f1b53f3df07230
TG-NAV <laibaidu@outlook.com>
2025-02-01 21:18:11 +0800

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -28,9 +28,10 @@ function generateSitemap(files) {
 
     files.forEach(function(file) {
         const url = 'https://tg-nav.github.io/clashnode/' + file.replace('./', ''); // Update URL as needed
-          const lastmod = new Date().toISOString().split('T')[0]; // Current date as lastmod
-        
-        xml += `\t<url>\n\t\t<loc>${url}</loc>\n\t</url>\n`;
+        const lastmod = new Date().toISOString().split('T')[0]; // Current date as lastmod
+       
+
+        xml += `\t<url>\n\t\t<loc>${url}</loc>\n\t\t<lastmod>${lastmod}</lastmod>\n\t\t</url>\n`;
     });
 
     xml += '</urlset>';
